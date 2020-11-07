351b3d360b9700df07c1e108795244093ffb3500
F.-Nicola Demers <fndemers@gmail.com>
2020-11-07 17:26:05 -0500

    Correction Docker pour clé USB

--- run.sh
+++ run.sh
@@ -9,6 +9,12 @@ apt install -y docker.io
 systemctl enable --now docker
 usermod -aG docker xubuntu
 
+echo "{" > /etc/docker/daemon.json
+echo "\"storage-driver\": \"vfs\"" >> /etc/docker/daemon.json
+echo "}" >> /etc/docker/daemon.json
+systemctl restart docker
+
+
 # Installation Docker compose
 apt install -y curl
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
