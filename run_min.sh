be8aa048b58459b3039bcd86ff0c8262d34e8246
Francois-N. Demers <fndemers@gmail.com>
2021-10-25 13:50:11 -0400

    Update run_min.sh
    Suppression d'une app.

--- run_min.sh
+++ run_min.sh
@@ -19,9 +19,6 @@ apt install -y curl
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Codium
-snap install --classic codium
-
 # Zoom
 wget https://zoom.us/client/latest/zoom_amd64.deb
 apt install -y ./zoom_amd64.deb
