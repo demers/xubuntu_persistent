98c29bd36fc35bba055961d62f072c72b040fd89
Francois-N. Demers <fndemers@gmail.com>
2021-11-22 08:01:35 -0500

    Update vm.sh
    Enlever docker-compose

--- vm.sh
+++ vm.sh
@@ -20,8 +20,8 @@ usermod -aG docker xubuntu
 
 # Installation Docker compose
 apt install -y curl
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+#curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
 
 # Interpréteur Fish
 apt install -y fish
